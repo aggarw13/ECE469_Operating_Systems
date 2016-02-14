8e97429bd6355fdf0508a09b9c6e64cc682b9667
Archit Aggarwal <aggarw13@purdue.edu>
2016-02-13 19:44:49 -0500

    q2 : Process create problem

---
+++ lab2/apps/apps/q4/include/circular_buffer.h
@@ -0,0 +1,14 @@
+#ifndef __USERPROG__
+#define __USERPROG__
+
+#define FILENAME_PROD "producer.dlx.obj"
+#define FILENAME_CONS "consumer.dlx.obj"
+
+typedef struct circ_buffer
+{
+	char mem_buffer[BUFFERSIZE];
+	int head;
+	int tail;
+};
+
+#endif
